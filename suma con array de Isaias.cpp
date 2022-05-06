08caa1a8b36b5aea1f86a23196f8998489226380
BarriosCifu <102176372+BarriosCifu@users.noreply.github.com>
2022-05-05 20:39:10 -0600

    Add files via upload

---
+++ suma con array de Isaias.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int  array [8] = {7,13,50,9,5,40,40,455};
+   for (int i=0;i<8;i++)
+{
+    cout<<array[i]<<", ";
+}
+   cout<<endl;
+   cout<<endl;
+
+        cout<<"la suma de las dos pocisiones";
+    cout<<array[2] + array[6];
+
+   return 0;
+}
